5fd0d496883f72ecc73ff2df776055c49a45edaa
Victor <victorml2204@gmail.com>
2024-03-19 18:03:14 +0100

    fe2

--- EjerciciosUD6_Medina_Lejeune_Victor/src/ejercicio03/Principal.java
+++ EjerciciosUD6_Medina_Lejeune_Victor/src/ejercicio03/Principal.java
@@ -1,9 +1,17 @@
 package ejercicio03;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		String aux;
+		boolean repetir = false;
+		do {
+
+		} while (repetir);
 	}
 
 }
